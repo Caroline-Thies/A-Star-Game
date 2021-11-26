b475bfef7125e9d54c5a2bf522631990711f6d12
caroline <carolinethies01@gmail.com>
2021-11-26 09:16:53 +0100

    call input reader in main

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import InputReader
+
 #read input
 
-#build first Graph
+INPUT_FILE_PATH = "Puzzle_6.CSV"
+START_NODE_X = 3
+START_NODE_Y = 4
+
+initial_nodeset, free_token = InputReader.get_nodeset_and_free_token_from_file(INPUT_FILE_PATH)
+start_node = initial_nodeset[START_NODE_Y][START_NODE_X]
+
+print(start_node)
 
 #give first node set to a*
-
-#do a*
 
 #write output
