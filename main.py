8e30d33be70c26292e0d9cedc610206c15da2ce2
caroline <carolinethies01@gmail.com>
2021-11-27 13:15:29 +0100

    remove commented out code

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import InputReader
 import AStar
-# read input
-from PriorityQueue import PriorityQueue
 
 INPUT_FILE_PATH = "Puzzle_6.CSV"
 START_NODE_ROW = 4
@@ -13,11 +11,15 @@ GOAL_NODE_COL = 2
 
 def print_path_edges(start_node, end_node):
     node = end_node
+    reversed_edges = []
     while node.path_in_edge is not None:
-        print(node.path_in_edge)
-        if not type(node.path_in_edge.movement) == type("  "):
-            print(node.path_in_edge.source.parent_set)
+        reversed_edges.append(node.path_in_edge)
         node = node.path_in_edge.source
+    edges = reversed(reversed_edges)
+    for edge in edges:
+        print(edge)
+        if not type(edge.movement) == type("  "):
+            print(edge.dest.parent_set)
 
 
 initial_nodeset = InputReader.get_nodeset_and_free_token_from_file(INPUT_FILE_PATH)
@@ -27,10 +29,5 @@ print(str(initial_nodeset))
 print("Free Token:")
 print(str(initial_nodeset.free_token))
 first_node = initial_nodeset.nodes[START_NODE_ROW][START_NODE_COL]
-# print("Start Node:\n" + str(first_node))
-# give first node set to a*
 final_node = AStar.a_star(first_node)
 print_path_edges(first_node, final_node)
-# for edge in edges_to_goal:
-#    print(str(edge.movement))
-# write output
